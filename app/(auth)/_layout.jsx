d58cf97d1daa23a1a7a98d59aff4e47edd20a91f
lopesdaluz <tlopesdaluz@gmail.com>
2025-04-04 15:24:01 +0200

    added the stack screen for layout of authenticaion

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" option={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" option={{ headerShown: false }} />
+      </Stack>
+      <StatusBar barStyle="light-content" backgroundColor="#161622" />
+    </>
   );
 };
 
