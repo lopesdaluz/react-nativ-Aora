2939d4ce8b52acce28bbbf431c30054a881f5f7f
lopesdaluz <tlopesdaluz@gmail.com>
2025-04-17 20:34:10 +0200

    skapat en UI för create a post men ingen funktionalitet än

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,11 +1,36 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, TextInput } from "react-native";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState(" ");
+
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#161622" }}>
+      <View style={{ padding: 60 }}>
+        <Text
+          style={{
+            fontSize: 20,
+            color: "#FFFFFF",
+            fontFamily: "Psemibold",
+            margin: 30,
+          }}
+        >
+          Create a post
+        </Text>
+        <TextInput
+          style={{
+            backgroundColor: "#FFFFFF",
+            marginVertical: 5,
+            color: "#000000",
+            fontFamily: "Pregular",
+          }}
+          value={title}
+          onChangeText={setTitle}
+          placeholder="Enter post title"
+          placeholderTextColor="#888888"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
